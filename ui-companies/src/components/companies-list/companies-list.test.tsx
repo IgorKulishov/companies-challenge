8bf1dd4b387e51b0e390f0d7128a916d3f42ccb6
Igor Kulishov <igorkulishov@gmail.com>
2021-10-28 08:20:43 -0400

    readme docs

--- ui-companies/src/components/companies-list/companies-list.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CompaniesList from './companies-list';
-
-test('renders learn react link', () => {
-  render(<CompaniesList />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- ui-companies/src/constants/environment.ts
+++
@@ -1,1 +0,0 @@
-export const apiUrl = "https://mnebty5mk8.execute-api.us-east-1.amazonaws.com/dev";
